6651364759a151fb933f716367bd6086ecce8deb
raulIxc85 <raul.ixc@gmail.com>
2024-09-20 23:08:57 -0600

    Se agrego componente admin layout, se mejoro sidebar y cambios en navbar

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/pilots', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('pilots');
+
+Route::get('/agencies', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('agencies');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
