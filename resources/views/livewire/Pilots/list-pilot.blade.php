70992362dff6b5ecf22222e04bb64ea46d9c50bf
raulIxc85 <raul.ixc@gmail.com>
2024-09-23 23:01:08 -0600

    Se cambiaro a iconos en lista de pilotos, se instalo sweet alert 2, para confirmar borrar un piloto

--- resources/views/livewire/Pilots/list-pilot.blade.php
+++ resources/views/livewire/Pilots/list-pilot.blade.php
@@ -10,9 +10,17 @@ new class extends Component {
     public function with()
     {
         return [
-            'pilots' => Pilot::paginate(10)
+            'pilots' => Pilot::where('status', true)->paginate(10)
         ];
-        
+    }
+
+    public function delete($pilotId)
+    {
+        $pilot = Pilot::findOrFail($pilotId);
+        $pilot->status = false;
+        $pilot->save();
+
+        session()->flash('message', 'Piloto eliminado exitosamente.');
     }
 }; ?>
 
@@ -62,14 +70,19 @@ new class extends Component {
                                             </td>
                                             <td
                                                 class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                                <a wire:navigate href="{{ route('pilots.edit', $pilot->id) }}"
-                                                    class="text-indigo-600 hover:text-indigo-900">
-                                                    Edit
-                                                </a>
-                                                <button wire:confirm="¿Esta seguro de borrar el piloto?"
-                                                    wire:click=""
+                                                <button wire:navigate href="{{ route('pilots.edit', $pilot->id) }}"
                                                     class="ml-2 text-indigo-600 hover:text-indigo-900">
-                                                    Delete
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                                        <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                                                    </svg>
+
+                                                </button>
+                                                <button onclick="confirmDelete('{{ $pilot->id }}')"
+                                                    class="ml-2 text-indigo-600 hover:text-indigo-900">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                                        <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
+                                                    </svg>
+
                                                 </button>
                                             </td>
                                         </tr>
@@ -92,3 +105,27 @@ new class extends Component {
         </div>
     </div>
 </div>
+
+<script>
+    function confirmDelete(pilotId) {
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "¡No podrás revertir esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminarlo!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                @this.call('delete', pilotId);
+                Swal.fire(
+                    'Eliminado!',
+                    'El piloto ha sido eliminado.',
+                    'success'
+                )
+            }
+        })
+    }
+</script>
+
