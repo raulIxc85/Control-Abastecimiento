94524f2928014a8e4fe9c65e3d8cc740d5907ee6
raulIxc85 <raul.ixc@gmail.com>
2024-09-24 22:56:58 -0600

    Se agrego vista a pilotos en navigation blade, se coloco de prueba ya que al ingresar al perfildaba error

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('pilots')" :active="request()->routeIs('pilots')">
+                    <x-nav-link :href="route('pilots.index')" :active="request()->routeIs('pilots')">
                         {{ __('Pilotos') }}
                     </x-nav-link>
                 </div>
