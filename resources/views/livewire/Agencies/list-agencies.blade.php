18fbb9aa2faf30a0476fac22e6d68b846e8cb7e8
raulIxc85 <raul.ixc@gmail.com>
2024-09-27 08:56:12 -0600

    Se agrego ordenamiento de agencias por nombre

--- resources/views/livewire/Agencies/list-agencies.blade.php
+++ resources/views/livewire/Agencies/list-agencies.blade.php
@@ -10,7 +10,9 @@ new class extends Component {
     public function with()
     {
         return [
-            'agencies' => Agency::where('status', true)->paginate(10)
+            'agencies' => Agency::where('status', true)
+                            ->orderBy('name', 'asc') 
+                            ->paginate(10)
         ];
     }
 
@@ -43,7 +45,7 @@ new class extends Component {
                 <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                     <a wire:navigate href=" {{ route('agencies.create') }}"
                         class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
-                        Agregar agencia
+                        Crear agencia
                     </a>
                 </div>
             </div>
